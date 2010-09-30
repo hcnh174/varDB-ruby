b087d1306651bd87859c4c46667050041efd2de7
hcnh174 <nelsonhayes4@gmail.com>
2010-10-01 00:19:18 +0900

    improved query grammar and pfam downloader

--- dev/pfam.rb
+++ dev/pfam.rb
@@ -3,26 +3,75 @@
 require 'open-uri'
 require 'pp'
 require 'net/ftp'
+require 'zlib'
 
-ftp = Net::FTP.new('ftp.sanger.ac.uk')
-ftp.login
-ftp.chdir('pub/databases/Pfam/releases/Pfam24.0/database_files/')
-files = ftp.list('n*')
-ftp.getbinaryfile('pfamA.txt.gz','pfamA.gz',1024)
+def download_pfam_files(filenames)
+  ftp = Net::FTP.new('ftp.sanger.ac.uk')
+  ftp.login
+  ftp.chdir('pub/databases/Pfam/releases/Pfam24.0/database_files/')
+  filenames.each do |filename|
+    if File.exists?(filename)
+      puts "file exists, skipping: #{filename}"
+      next
+    end
+    puts "downloading pfam file #{filename}"
+    ftp.getbinaryfile(filename,filename,1024)
+  end
+end
 
-#
-#filename = 'pfamA.txt.gz'
-#open("ftp://ftp.sanger.ac.uk/pub/databases/Pfam/releases/Pfam24.0/database_files/#{filename}") do |file|
-#  puts file
-#end
-#
-#
-#Zlib::GzipReader.open('pfamA.txt.gz') {|gz|
-#  print gz.read
-#}
+def unzip_files(filenames)
+  filenames.each do |filename|
+    unzip_file(filename)
+  end
+end
 
-#File.open('hoge.gz') do |f|
-#  gz = Zlib::GzipReader.new(f)
-#  print gz.read
-#  gz.close
-#end
+def unzip_file(filename)
+  if !File.exists?(filename)
+    return
+  end
+  newfilename = filename.chop.chop.chop # take off .gz extension
+  puts newfilename
+  Zlib::GzipReader.open(filename) {|gz|
+    File.open(newfilename, 'w') do |outfile|  
+      outfile.puts gz.read  
+    end
+  }
+end
+
+def load_pfam_file(filename)
+  IO.foreach(filename) do |line|
+      line.chomp!
+      values=line.split("\t")
+      values = values.collect {|value| value.reverse.chop.reverse.chop }
+      hash = Hash.new
+      hash[:auto_pfama] = values[0]
+      hash[:identifier] = values[2] #pfama_id
+      hash[:name] = values[1] #pfamA_acc
+      hash[:description] = values[4]
+      hash[:type] = values[8]
+      #hash[:clan] = values[0] #clans.clan_id
+      puts hash
+  end
+end
+
+def load_clan_file(filename)
+  IO.foreach(filename) do |line|
+      line.chomp!
+      values=line.split("\t")
+      values = values.collect {|value| value.reverse.chop.reverse.chop }
+      hash = Hash.new
+      hash[:auto_clan] = values[0]
+      hash[:identifier] = values[2] #clan_id
+      hash[:name] = values[1] #clan_acc
+      hash[:description] = values[4] #clan_description
+      puts hash
+  end
+end
+
+filenames = %w{clans.txt.gz pfamA.txt.gz clan_membership.txt.gz}
+
+#download_pfam_files(filenames)
+#unzip_files(filenames)
+#load_pfam_file('pfamA.txt')
+load_clan_file('clans.txt')
+
--- dev/testquery.rb
+++ dev/testquery.rb
@@ -1,13 +1,63 @@
 require 'treetop'
 require 'pp'
 
-Treetop.load 'query2'
+class Filter
+  
+  def text
+    raise 'This method should be overriden in a derived class'
+  end
+end
+ 
 
-query = 'family="var" OR family="rif"'
+class ElementaryFilter < Filter
+
+  attr_accessor :field, :operator, :value
+ 
+#  def initialize(field, operator, value)
+#    super
+#    @field=field
+#    @operator=operator
+#    @value=value
+#  end
+  
+  def text
+    return "#{@field}#{@operator}#{@value}"
+  end
+  
+end
+
+class CompositeFilter < Filter
+
+  attr_accessor :left, :operator, :right
+    
+#  def initialize(left, operator, right)
+#    super
+#    @left=left
+#    @operator=operator
+#    @right=right
+#  end
+  
+  def text
+    return "#{@left.text} #{@operator} (#{@right.text})"
+  end
+end
+
+#filter = CompositeFilter.new(nil,'AND',nil)
+
+#filter1 = ElementaryFilter.new('family','=','var')
+#filter2 = ElementaryFilter.new('family','=','rif')
+#filter = CompositeFilter.new(filter1,'OR',filter2)
+#puts filter.text
+
+
+Treetop.load 'query3'
+
+#query = 'family="var" OR family="rif"'
 #query = 'family="var"'
 #query = '"dinner"'
 #query = '>='
 #query = 'family="var"'
+query = 'family="var" AND family="rif"'
 #query = 'family'
 
 #puts query
@@ -15,9 +65,9 @@ parser = QueryParser.new
 result = parser.parse(query)
 #pp result
 #puts dumptree(result)
-pp result.content
 if result
-  puts 'success'  
+  puts 'success'
+  pp result.content
 else
   puts 'failure'
 end
